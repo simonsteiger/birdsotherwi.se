e283a612ec23277ddd24bc13df11b20002aee4ca
simonsteiger <simon.j.steiger@gmail.com>
2025-01-12 08:41:54 +0100

    progress on posts and md-layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
-
 import remarkMath from 'remark-math';
-
 import rehypeKatex from 'rehype-katex';
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind(), mdx()],
   markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark'
+      },
+    },
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
